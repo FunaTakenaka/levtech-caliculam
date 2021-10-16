8bfdcb48cdc8cb72474e43fa3c0901002737bd52
FunaTakenaka <s19B2075VQ@s.chibakoudai.jp>
2021-10-17 00:28:45 +0900

    09-4

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -11,43 +11,28 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
-    }
+        // クライアントインスタンス生成
+        $client = new \GuzzleHttp\Client();
 
-    public function show(Post $post)
-    {
-        return view('show')->with(['post' => $post]);
-    }
+        // GET通信するURL
+        $url = 'https://teratail.com/api/v1/questions';
 
-    public function create()
-    {
-        return view('create');
-    }
-
-    public function store(Post $post, PostRequest $request) 
-    {
-        $input = $request['post'];
-        $input += ['user_id' => $request->user()->id];
-        $post->fill($input)->save();
-        return redirect('/posts/' . $post->id);
-    }
-    
-    public function edit(Post $post)
-    {
-        return view('edit')->with(['post' => $post]);
-    }
-    
-    public function update(Post $post, PostRequest $request)
-    {
-        $input_post = $request['post'];
-        $input_post += ['user_id' => $request->user()->id];
-        $post->fill($input_post)->save();
-        return redirect('/posts/' . $post->id);
-    }
-    
-    public function delete(Post $post)
-    {
-        $post->delete();
-        return redirect('/');
+        // リクエスト送信と返却データの取得
+        // Bearerトークンにアクセストークンを指定して認証を行う
+        $response = $client->request(
+            'GET',
+            $url,
+            ['Bearer' => config('services.teratail.token')]
+        );
+        
+        // API通信で取得したデータはjson形式なので
+        // PHPファイルに対応した連想配列にデコードする
+        $questions = json_decode($response->getBody(), true);
+        
+        // index bladeに取得したデータを渡す
+        return view('index')->with([
+            'posts' => $post->getPaginateByLimit(),
+            'questions' => $questions['questions'],
+        ]);
     }
 }
--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -1,6 +1,3 @@
-@extends('layouts.app')　　　　　　　　　　　　　　　　　　
-@section('content')
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -10,27 +7,14 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        @if(Auth::check())
-        {{Auth::user()->name}}
-        @else
-        @endif
-        <h1>Blog Name</h1>
-        <p class = 'create'>[<a href = "posts/create">create</a>]</p>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h3><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h3>
-                    <small>{{ $post->user->name }}</small>
-                    <p class='body'>{{ $post->body }}</p>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div>
+        @foreach($questions as $question)
+            <div>
+              <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                {{ $question['title'] }}
+              </a>
+             </div>
+        @endforeach
+    </div>
     </body>
 </html>
-@endsection
